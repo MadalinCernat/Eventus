cb88b0971834ebcec0a86480721124a08bfee378
MadalinCernat <madalin.cernat02@gmail.com>
2022-01-13 21:33:33 +0200

    Added a class for the SQL crud with a GetAll method for events.

---
+++ src/DataAccessLibrary/Models/EventModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary.Models
+{
+    public class EventModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public int PlaceId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public decimal EntranceTax { get; set; }
+        public string CreatedByUserId { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
---
+++ src/DataAccessLibrary/SqlCrud.cs
@@ -0,0 +1,23 @@
+﻿using DataAccessLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class SqlCrud
+    {
+        private readonly ISqlDataAccess _db;
+
+        public SqlCrud(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+        public Task<List<EventModel>> GetAllEvents()
+        {
+            return _db.LoadData<EventModel, dynamic>("dbo.spEvent_GetAll", new { }, true);
+        }
+    }
+}
