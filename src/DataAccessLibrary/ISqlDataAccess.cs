e519a04362c1c1bef15f1ffd7e54c4d66f124f27
MadalinCernat <madalin.cernat02@gmail.com>
2022-01-13 21:30:27 +0200

    Added dapper sql data access methods

---
+++ src/DataAccessLibrary/ISqlDataAccess.cs
@@ -0,0 +1,9 @@
+﻿
+namespace DataAccessLibrary
+{
+    public interface ISqlDataAccess
+    {
+        Task<List<T>> LoadData<T, U>(string sql, U parameters, bool isStoredProcedure = false);
+        Task SaveData<T>(string sql, T parameters, bool isStoredProcedure = false);
+    }
+}
---
+++ src/DataAccessLibrary/SqlDataAccess.cs
@@ -0,0 +1,48 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class SqlDataAccess : ISqlDataAccess
+    {
+        private readonly string _connectionString;
+
+        public SqlDataAccess(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public async Task<List<T>> LoadData<T, U>(string sql, U parameters, bool isStoredProcedure = false)
+        {
+            var output = new List<T>();
+            CommandType commandType = CommandType.Text;
+            if (isStoredProcedure)
+            {
+                commandType = CommandType.StoredProcedure;
+            }
+            using (IDbConnection conn = new SqlConnection(_connectionString))
+            {
+                output = (await conn.QueryAsync<T>(sql, parameters, commandType: commandType)).ToList();
+            }
+            return output;
+        }
+
+        public async Task SaveData<T>(string sql, T parameters, bool isStoredProcedure = false)
+        {
+            CommandType commandType = CommandType.Text;
+            if (isStoredProcedure)
+            {
+                commandType = CommandType.StoredProcedure;
+            }
+            using (IDbConnection conn = new SqlConnection(_connectionString))
+            {
+                await conn.ExecuteAsync(sql, parameters, commandType: commandType);
+            }
+        }
+    }
+}
