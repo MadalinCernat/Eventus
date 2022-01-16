447354427678a360965f723b81d7bcd362dba374
MadalinCernat <madalin.cernat02@gmail.com>
2022-01-16 23:02:12 +0200

    Modified EventModel to contain models for other entities.

--- src/DataAccessLibrary/Models/EventModel.cs
+++ src/DataAccessLibrary/Models/EventModel.cs
@@ -11,7 +11,7 @@ namespace DataAccessLibrary.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string? Description { get; set; }
-        public int PlaceId { get; set; }
+        public PlaceModel Place { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
 
---
+++ src/DataAccessLibrary/Models/PlaceModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary.Models
+{
+    public class PlaceModel
+    {
+        public int Id { get; set; }
+        public CityModel City { get; set; }
+    }
+}
