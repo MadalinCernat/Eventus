099b1eba6ba6039d57a97feb3dea39bd35484ab8
MadalinCernat <madalin.cernat02@gmail.com>
2022-01-16 22:40:51 +0200

    Added Id columns to the select statement

--- src/EventusDb/dbo/StoredProcedures/spEvent_GetById.sql
+++ src/EventusDb/dbo/StoredProcedures/spEvent_GetById.sql
@@ -5,7 +5,7 @@ begin
 	set nocount on;
 
 	select [e].[Id], [e].[Title], [e].[Description], [e].[StartDate], [e].[EndDate], [e].[EntranceTax], [e].[CreatedByUserId],
-		   [e].[DateCreated], [e].[IsActive], [e].[IsOver], [e].[Url], [p].[Name], [c].[Name]
+		   [e].[DateCreated], [e].[IsActive], [e].[IsOver], [e].[Url], [p].Id, [p].[Name], [c].[Id], [c].[Name]
 	from dbo.[Event] e
 	inner join dbo.Place p on p.Id = e.PlaceId
 	inner join dbo.City c on c.Id = p.CityId
