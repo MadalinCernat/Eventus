8e6f16559ee27c34085352f1ed0839ec4bcc003f
hartkemd <hartkemd@gmail.com>
2022-01-16 00:16:10 -0500

    Added stored procedure for getting event by Id.

---
+++ src/EventusDb/dbo/StoredProcedures/spEvent_GetById.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[spEvent_GetById]
+	@Id int
+AS
+begin
+	set nocount on;
+
+	select [e].[Id], [e].[Title], [e].[Description], [e].[StartDate], [e].[EndDate], [e].[EntranceTax], [e].[CreatedByUserId],
+		   [e].[DateCreated], [e].[IsActive], [e].[IsOver], [e].[Url], [p].[Name], [c].[Name]
+	from dbo.[Event] e
+	inner join dbo.Place p on p.Id = e.PlaceId
+	inner join dbo.City c on c.Id = p.CityId
+	where e.Id = @Id;
+end
