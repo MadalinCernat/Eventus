be8b41acea2944db451f6209d089dba63c906fa7
MadalinCernat <madalin.cernat02@gmail.com>
2022-01-17 15:49:06 +0200

    Added new models for invitation and request

---
+++ src/DataAccessLibrary/Models/InvitationModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary.Models
+{
+    public class InvitationModel
+    {
+        public int Id { get; set; }
+        public string SentByUserId { get; set; }
+        public string SentToUserId { get; set; }
+        public EventModel Event { get; set; }
+        public bool Accepted { get; set; }
+        public DateTime DateSent { get; set; }
+        public bool IsActive { get; set; }
+        public string Message { get; set; }
+        public bool Responded { get; set; }
+
+    }
+}
---
+++ src/DataAccessLibrary/Models/RequestModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary.Models
+{
+    public class RequestModel
+    {
+        public int Id { get; set; }
+        public string SentByUserId { get; set; }
+        public EventModel Event { get; set; }
+        public string RequestMessage { get; set; }
+        public DateTime Date { get; set; }
+        public bool Accepted { get; set; }
+    }
+}
