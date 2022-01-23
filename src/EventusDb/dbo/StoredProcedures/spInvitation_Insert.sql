1d81b7d070cd5bdec8f9b09d3470b10c9931c376
MadalinCernat <madalin.cernat02@gmail.com>
2022-01-23 19:14:19 +0200

    Added stored procedure for invitation insert.

---
+++ src/EventusDb/dbo/StoredProcedures/spInvitation_Insert.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[spInvitation_Insert]
+	@SentByUserId nvarchar(450),
+	@SentToUserId nvarchar(450),
+	@EventId int,
+	@DateSent datetime2(7),
+	@Message nvarchar(200)
+AS
+begin
+	set nocount on;
+	insert into [dbo].Invitation
+	([SentByUserId], [SentToUserId], [EventId], [DateSent], [Message])
+	values (@SentByUserId, @SentToUserId, @EventId, @DateSent, @Message)
+end
