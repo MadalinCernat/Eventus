b97ccad942661fa0512ac2f4edda93b1d93f9ad6
MadalinCernat <madalin.cernat02@gmail.com>
2022-01-23 18:55:03 +0200

    Added stored procedure for event insertion.

---
+++ src/EventusDb/dbo/StoredProcedures/spEvent_Insert.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[spEvent_Insert]
+	@Title nvarchar(200),
+	@Description nvarchar(1000),
+	@PlaceId int,
+	@StartDateTime datetime2(7),
+	@EndDateTime datetime2(7),
+	@EntranceFee money,
+	@CreatedByUserId nvarchar(450),
+	@DateCreated datetime2(7),
+	@Url nvarchar(1000),
+	@AllowRequests bit
+AS
+begin
+	set nocount on;
+	insert into [dbo].[Event]
+	([Title], [Description], [PlaceId], [StartDateTime], [EndDateTime], [EntranceFee], [CreatedByUserId], [DateCreated], [Url], [AllowRequests])
+	values (@Title, @Description, @PlaceId, @StartDateTime, @EndDateTime, @EntranceFee, @CreatedByUserId, @DateCreated, @Url, @AllowRequests)
+end
