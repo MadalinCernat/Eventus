8d9ea1466517189c626128aa04befa4c1d2a295e
MadalinCernat <madalin.cernat02@gmail.com>
2022-01-23 19:24:48 +0200

    Added store procedure for user city insert.

---
+++ src/EventusDb/dbo/StoredProcedures/spUserCity_Insert.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spUserCity_Insert]
+	@UserId nvarchar(450),
+	@CityId int
+as
+begin
+	set nocount on;
+	insert into [dbo].[UserCity]
+	([UserId], [CityId])
+	values (@UserId, @CityId)
+end
