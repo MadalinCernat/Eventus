5aa47cddc4f7187861e2c8e685b610369997b14d
MadalinCernat <madalin.cernat02@gmail.com>
2022-01-23 19:34:30 +0200

    Added stored procedure for event user insert.

---
+++ src/EventusDb/dbo/StoredProcedures/spEventUser_Insert.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spEventUser_Insert]
+	@UserId nvarchar(450),
+	@EventId int
+as
+begin
+	set nocount on;
+	insert into [dbo].EventUser
+	([UserId], [EventId])
+	values (@UserId, @EventId)
+end
--- src/EventusDb/dbo/Tables/EventUsers.sql
+++ src/EventusDb/dbo/Tables/EventUsers.sql
@@ -1,6 +1,6 @@
-﻿CREATE TABLE [dbo].[EventUsers]
+﻿CREATE TABLE [dbo].[EventUser]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [UserId] NVARCHAR(450) NOT NULL, 
     [EventId] INT NOT NULL, 
     CONSTRAINT [FK_EventUsers_Event] FOREIGN KEY (EventId) REFERENCES [dbo].[Event](Id)
