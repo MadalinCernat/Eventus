2ec3c6bdc99e292615d6895c94fa38ae9ea87586
hartkemd <hartkemd@gmail.com>
2022-01-23 09:06:47 -0500

    Added stored procedure for getting invitations sent to a user.

---
+++ src/EventusDb/dbo/StoredProcedures/spInvitation_GetAllSentToUserId.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[spInvitation_GetAllSentToUserId]
+	@sentToUserId nvarchar(450)
+AS
+begin
+	set nocount on;
+
+	select i.*, e.*
+	from dbo.Invitation i
+	inner join dbo.[Event] e on e.Id = i.EventId
+	where i.SentToUserId = @sentToUserId;
+end
