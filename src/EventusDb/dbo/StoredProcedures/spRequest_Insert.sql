acf449a68be09d51d165a4c8973590531f6b2a55
MadalinCernat <madalin.cernat02@gmail.com>
2022-01-23 19:18:46 +0200

    Added stored procedure for request insert.

---
+++ src/EventusDb/dbo/StoredProcedures/spRequest_Insert.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[spRequest_Insert]
+	@SentByUserId nvarchar(450),
+	@ForEventId int,
+	@RequestMessage nvarchar(200),
+	@Date datetime2(7)
+AS
+begin
+	set nocount on;
+
+	insert into [dbo].[Request]
+	([SentByUserId], [ForEventId], [RequestMessage], [Date])
+	values (@SentByUserId, @ForEventId, @RequestMessage, @Date)
+end
