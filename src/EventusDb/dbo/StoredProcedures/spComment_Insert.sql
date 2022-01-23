e705bfd8e901c08cab13819bb73a8f41809a555d
MadalinCernat <madalin.cernat02@gmail.com>
2022-01-23 19:38:32 +0200

    Added stored procedure for comment insert.

---
+++ src/EventusDb/dbo/StoredProcedures/spComment_Insert.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[spComment_Insert]
+	@Content nvarchar(200),
+	@PostedByUserId nvarchar(450),
+	@DatePosted datetime2(7),
+	@EventId int
+as
+begin
+	set nocount on;
+	insert into [dbo].Comment
+	([Content], [PostedByUserId], [DatePosted], [EventId])
+	values (@Content, @PostedByUserId, @DatePosted, @EventId)
+end
