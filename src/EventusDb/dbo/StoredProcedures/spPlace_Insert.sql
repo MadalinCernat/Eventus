9ee6212d4701d89b00b43564d8c31d05d652edba
MadalinCernat <madalin.cernat02@gmail.com>
2022-01-23 19:29:56 +0200

    Added stored procedure for place insert.

---
+++ src/EventusDb/dbo/StoredProcedures/spPlace_Insert.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spPlace_Insert]
+	@Name nvarchar(100),
+	@CityId int
+as
+begin
+	set nocount on;
+	insert into [dbo].[Place]
+	([Name], [CityId])
+	values (@Name, @CityId)
+end
