728a2fe6f14832b6b96fc22316415eddfb92b67f
hartkemd <hartkemd@gmail.com>
2022-01-23 09:38:44 -0500

    Expanded wildcards in several of my stored procedures.

--- src/EventusDb/dbo/StoredProcedures/spInvitation_GetAllSentToUserId.sql
+++ src/EventusDb/dbo/StoredProcedures/spInvitation_GetAllSentToUserId.sql
@@ -4,7 +4,9 @@ AS
 begin
 	set nocount on;
 
-	select i.*, e.*
+	select [i].[Id], [i].[SentByUserId], [i].[SentToUserId], [i].[EventId], [i].[Accepted], [i].[DateSent], [i].[IsActive],
+		[e].[Id], [e].[Title], [e].[Description], [e].[PlaceId], [e].[StartDate], [e].[EndDate], [e].[EntranceTax],
+		[e].[CreatedByUserId], [e].[DateCreated], [e].[IsActive], [e].[IsOver], [e].[Url]
 	from dbo.Invitation i
 	inner join dbo.[Event] e on e.Id = i.EventId
 	where i.SentToUserId = @sentToUserId;
