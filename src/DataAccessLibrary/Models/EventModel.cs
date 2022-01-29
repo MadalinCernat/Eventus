26061afee58ae3a6063b3cadef4c54578e131915
MadalinCernat <madalin.cernat02@gmail.com>
2022-01-29 15:40:51 +0200

    Modified event model.

--- src/DataAccessLibrary/Models/EventModel.cs
+++ src/DataAccessLibrary/Models/EventModel.cs
@@ -12,10 +12,10 @@ namespace DataAccessLibrary.Models
         public string Title { get; set; }
         public string? Description { get; set; }
         public PlaceModel Place { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
 
-        public decimal EntranceTax { get; set; }
+        public decimal EntranceFee { get; set; }
         public string CreatedByUserId { get; set; }
         public DateTime DateCreated { get; set; }
         public bool IsActive { get; set; }
