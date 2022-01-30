f325d8a727b677cb1437c52508c51606657fe80b
MadalinCernat <madalin.cernat02@gmail.com>
2022-01-30 21:58:39 +0200

    Created stored procedure to get place by id.

---
+++ src/EventusDb/dbo/StoredProcedures/spPlace_GetById.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[spPlace_GetById]
+	@Id int
+AS
+begin
+	set nocount on;
+
+	select [p].[Id], [p].[Name], [c].[Id] as 'CityId', [c].[Name]
+	from dbo.Place p
+	inner join dbo.City c on c.Id = p.CityId
+	where p.Id = @Id
+end
