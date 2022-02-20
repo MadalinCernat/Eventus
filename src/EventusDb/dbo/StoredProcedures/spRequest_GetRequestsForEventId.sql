0e556f94801daebeadac4d1be9fba5c8e46029fc
MadalinCernat <madalin.cernat02@gmail.com>
2022-02-20 11:39:58 +0200

    Made inner joins to include everything.

--- src/EventusDb/dbo/StoredProcedures/spRequest_GetRequestsForEventId.sql
+++ src/EventusDb/dbo/StoredProcedures/spRequest_GetRequestsForEventId.sql
@@ -4,10 +4,30 @@ AS
 begin
 	set nocount on;
 	
-	select [r].[Id], [r].[SentByUserId], [r].[ForEventId], [r].[RequestMessage], [r].[Date], [r].[Accepted],
-		[e].[Id], [e].[Title], [e].[Description], [e].[PlaceId], [e].[StartDateTime], [e].[EndDateTime], [e].[EntranceFee],
-		[e].[CreatedByUserId], [e].[DateCreated], [e].[IsActive], [e].[Url], [e].AllowRequests
+	select [r].[Id]
+		, [r].[SentByUserId]
+		, [r].[ForEventId]
+		, [r].[RequestMessage]
+		, [r].[Date]
+		, [r].[Accepted]
+		, [e].[Id] as 'EventId'
+		, [e].[Title]
+		, [e].[Description]
+		, [e].[StartDateTime]
+		, [e].[EndDateTime]
+		, [e].[EntranceFee]
+		, [e].[CreatedByUserId]
+		, [e].[DateCreated]
+		, [e].[IsActive]
+		, [e].[Url]
+		, [e].[AllowRequests]
+		, [p].[Id] as 'PlaceId'
+		, [p].[Name]
+		, [c].[Id] as 'CityId'
+		, [c].[Name]
 	from dbo.Request r
-	inner join dbo.[Event] e on e.Id = r.ForEventId
+	inner join [dbo].[Event] e on e.Id = r.ForEventId
+	inner join [dbo].[Place] p on p.Id = e.PlaceId
+	inner join [dbo].[City] c on c.Id = p.CityId
 	where r.ForEventId = @eventId;
 end
